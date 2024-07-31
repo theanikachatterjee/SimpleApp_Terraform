b1c906b317d15f2b5176c190cc955a312f702a01
ANIKA CHATTERJEE <anikachatterjeeo612@gmail.com>
2024-07-31 17:17:47 +0530

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "fitnessgeek-storage" {
-  name                     = "fgstorageaccountbyanika051989"
+  name                     = "fgstorageanika051989"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
