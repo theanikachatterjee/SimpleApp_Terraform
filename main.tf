8b57a72bf17d8374956b033b897bbffc48f730a6
ANIKA CHATTERJEE <anikachatterjeeo612@gmail.com>
2024-07-31 16:40:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = "Fitnessgeek-rg"
+  name     = "FFitnessgeek-rg"
   location = var.location-rg
   tags = {
     "Application" = "DemoApp"
